b44cf239f8cd129d3e58a8344c7435759c2792aa
Oscar-MH <oscarmartinez07082005@gmail.com>
2025-05-12 22:51:29 -0600

    Add files via upload

---
+++ calculadora-cientifica/src/main/java/com/upiiz/calculadora_C/CalculadoraCApplication.java
@@ -0,0 +1,13 @@
+package com.upiiz.calculadora_C;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraCApplication.class, args);
+	}
+
+}
---
+++ calculadora-cientifica/src/test/java/com/upiiz/calculadora_C/CalculadoraCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upiiz.calculadora_C;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
